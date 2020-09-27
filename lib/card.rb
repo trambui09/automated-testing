69c0328a2d095cd7e01b4302f577c0cb34315eaf
Tram Bui <pbui17@cmc.edu>
2020-09-26 20:40:44 -0500

    Passed wave 1 and wave 2

--- lib/card.rb
+++ lib/card.rb
@@ -1,18 +1,52 @@
-
 # card.rb
+
+VALID_SUITS = [:hearts, :spades, :clubs, :diamonds]
 
 class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    unless VALID_SUITS.include?(suit)
+      raise ArgumentError.new("#{suit} is an invalid suit")
+    end
+
+    unless (1..13).include?(value)
+      raise ArgumentError.new("#{value} is an invalid value")
+    end
+
     @value = value
     @suit = suit
-    
+
+    #should this be its own method?
+
 
   end
 
+  # def valid_params(input)
+  #   if ![:hearts, :spades, :clubs, :diamonds].include?(suit) || !(1..13).include?(value)
+  #     raise ArgumentError.new("#{suit} or #{value} is an invalid input")
+  #   end
+  # end
+
   def to_s
-    return "#{value} of #{suit.to_s}"
+    return "#{get_face_card(value)} of #{suit.to_s}"
+  end
+  # value as the input, changed value based on face cards as the output
+  def get_face_card(value)
+
+    case value
+    when 1
+      "Ace"
+    when 11
+      "Jack"
+    when 12
+      "Queen"
+    when 13
+      "King"
+    else
+      value
+    end
+
   end
 
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -26,6 +26,20 @@ describe Card do
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+
+      card_1 = Card.new(2, :diamonds)
+      card_2 = Card.new(3, :clubs)
+      card_3 = Card.new(10, :hearts)
+      card_4 = Card.new(9, :spades)
+
+      card_arrays = [card_1, card_2, card_3, card_4]
+
+      card_arrays.each do |card|
+        expect(card.to_s).must_equal "#{card.value} of #{card.suit.to_s}"
+      end
+
+      #expect(card_1.to_s).must_equal "2 of diamonds"
+
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
@@ -40,6 +54,30 @@ describe Card do
       #  11: Jack
       #  12: Queen
       #  13: King
+
+      # card_1 = Card.new(11, :diamonds)
+      # card_2 = Card.new(1, :clubs)
+      # card_3 = Card.new(12, :spades)
+      # card_4 = Card.new(13, :hearts)
+      #
+      # expect(card_1.to_s).must_equal "Jack of diamonds"
+      # expect(card_2.to_s).must_equal "Ace of clubs"
+      # expect(card_3.to_s).must_equal "Queen of spades"
+      # expect(card_4.to_s).must_equal "King of hearts"
+
+      # refactored
+      face_cards = {
+          1 => "Ace",
+          11 => "Jack",
+          12 => "Queen",
+          13 => "King"
+      }
+
+      face_cards.each do |number, face_value|
+        card = Card.new(number, :diamonds)
+        expect(card.to_s).must_equal "#{face_value} of diamonds"
+      end
+
     end
   end
 
@@ -47,10 +85,22 @@ describe Card do
 
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
+      card_3 = Card.new(10, :hearts)
+
+      expect(card_3.value).must_equal 10
+      expect(Card.new(13, :hearts).value).must_equal 13
+      expect(Card.new(11, :hearts).value).must_equal 11
+
+
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
+      card_3 = Card.new(10, :hearts)
+
+      expect(card_3.suit).must_equal :hearts
+      expect(Card.new(13, :diamonds).suit).must_equal :diamonds
+      expect(Card.new(13, :hearts).suit).must_equal :hearts
 
     end
   end
